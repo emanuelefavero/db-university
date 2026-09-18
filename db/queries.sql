c892c77d0ff5d5afd1e8992b843abbfb025c01d8
Emanuele Favero <emafaux89@gmail.com>
2026-09-18 12:34:09 +0200

    feat: add query 11 - calculate average grade for each exam session

--- db/queries.sql
+++ db/queries.sql
@@ -79,3 +79,12 @@ from
   teachers
 group by
   office_address;
+
+-- Calculate the average grade for each exam session
+select
+  exam_id,
+  round(avg(vote), 2) as average_grade
+from
+  exam_student
+group by
+  exam_id;

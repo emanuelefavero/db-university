eaf28ac1831240a76b7a4cb71ee727a2f250131d
Emanuele Favero <emafaux89@gmail.com>
2026-09-19 17:50:14 +0200

    feat: add join query 2

--- db/queries-join.sql
+++ db/queries-join.sql
@@ -46,4 +46,17 @@ from
   students s
   join degrees d on s.degree_id = d.id
 where
-  d.name = 'Corso di Laurea in Economia'
+  d.name = 'Corso di Laurea in Economia';
+
+-- 2. Select all "Corso di Laurea Magistrale" in "Dipartimento di Neuroscienze"
+select
+  c.name course_name,
+  d.name degree_name,
+  dep.name department_name
+from
+  courses c
+  join degrees d on c.degree_id = d.id
+  join departments dep on d.department_id = dep.id
+where
+  d.name like '%laurea magistrale%'
+  and dep.name = 'dipartimento di neuroscienze';

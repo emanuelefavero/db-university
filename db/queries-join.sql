059997cb4b8e6584bb26122087d6b112f0091c20
Emanuele Favero <emafaux89@gmail.com>
2026-09-19 16:35:58 +0200

    feat: add group by query 2

--- db/queries-join.sql
+++ db/queries-join.sql
@@ -6,3 +6,12 @@ from
   students
 group by
   year;
+
+-- Count how many teachers have their office in the same building
+select
+  office_address,
+  count(id) teacher_count
+from
+  teachers
+group by
+  office_address;
